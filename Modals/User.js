f5eff6f939461567154302075e86ec535c1a6abe
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-21 20:52:04 +0530

    Started registration

---
+++ Modals/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const path = require('path')
+
+dotenv.config({
+    path:path.join(__dirname,'../','.env')
+})
+
+// console.log(process.env.CONNECTION_URI)
+mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(() => {
+        console.log('Successfully connected to the database')
+    })
+    .catch((err) => {
+        console.log('Error ' + err)
+    })
+const User = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports = mongoose.model('Users',User)
+
