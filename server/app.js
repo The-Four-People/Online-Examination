53cef718c936214f5da1820d3e883bc744a16501
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-21 20:52:04 +0530

    Started registration

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const Register = require('./Routes/Register')
+const cors = require('cors')
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use('/register',Register)
+
+
+app.get('/',(req,res) => {
+    res.status(404).send("404 not found")
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log('App listening on port ' + PORT))
