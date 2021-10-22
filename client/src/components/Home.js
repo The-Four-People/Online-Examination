b34f5a2dd065e141a0a2f87a019b64ca1a27778d
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-22 19:54:11 +0530

    Small change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Hello this is Home of GPM site 😆</h1>
+            <h2><Link to='/login'>Login</Link></h2>
+            <h2><Link to='/register'>Register</Link></h2>
         </div>
     )
 }
