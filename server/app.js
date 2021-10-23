cec1802325d41c61152e257b20d064daef83e5b5
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-23 10:24:12 +0530

    Added a logger

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-
+app.use(logger)
 
 
 app.use('/register',Register)
@@ -17,6 +17,19 @@ app.use('/register',Register)
 app.get('/',(req,res) => {
     res.status(404).send("404 not found")
 })
+
+
+function logger(req,res,next){
+    const log = {
+        ip:req.ip,
+        url:req.url,
+        method:req.method,
+    }
+    console.log()
+    console.log(log)
+    console.log()
+    next()
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log('App listening on port ' + PORT))
