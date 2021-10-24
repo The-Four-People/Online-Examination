e12c82843f6c16b4c3ea2c9b66c9d6583b9a2cb6
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-24 17:52:19 +0530

    Added validation to email and password inputs

--- Routes/Register.js
+++ Routes/Register.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const UserModal = require('../Modals/User')
 const bcrypt = require('bcrypt')
+const { body, validationResult } = require('express-validator')
 const Router = express.Router()
 const app = express()
 
@@ -27,25 +28,49 @@ function save(name, email, password) {
                 reject({ ok: false, msg: 'Error', error: err })
             })
         } catch (err) {
-            reject({ok:false,msg:"Error",error:err})
+            reject({ ok: false, msg: "Error", error: err })
         }
     })
     return promise
 }
 
-Router.post('/', (req, res) => {
-    const { name, email, password } = req.body
-    save(name, email, password)
-        .then((data, err) => {
-            err ? res.json(err) : res.json(data)
-        })
-        .catch(err => {
-            res.json({ ok: false, msg: 'An error occured', error: err })
-        })
-})
+const validation = (req, res, next) => {
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        errors = errors.errors
+        const length = errors.length
+        if (length > 1) {
+            return res.status(400).json({
+                ok: false, msg: "Not a valid email address and " +
+                    "password should be atleast 5 characters long", error: errors
+            })
+        } else {
+            if (errors[0].param === "email") {
+                return res.status(400).json({ ok: false, msg: "Not an valid email address", error: errors })
+            } else {
+                return res.status(400).json({ ok: false, msg: "Password should be more than 5 characters", error: errors })
+            }
+        }
+    }
+    next()
+}
+Router.post('/',
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+    validation,
+    (req, res) => {
+        const { name, email, password } = req.body
+        save(name, email, password)
+            .then((data, err) => {
+                err ? res.json(err) : res.json(data)
+            })
+            .catch(err => {
+                res.json({ ok: false, msg: 'An error occured', error: err })
+            })
+    })
 
 Router.get('/', (req, res) => {
-    res.status(404).send("404 NOT FOUND")
+    res.sendStatus(404) 
 })
 
 
