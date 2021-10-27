604c7882ec9c24d2659af78140a595fa52a4aea0
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-27 17:35:49 +0530

    Trial

--- server/Modals/User.js
+++ server/Modals/User.js
@@ -7,7 +7,7 @@ dotenv.config({
 })
 
 // console.log(process.env.CONNECTION_URI)
-mongoose.connect(`${process.env.CONNECTION_URI}developers${process.env.CONNECTION_SUFFIX}`,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(`${process.env.CONNECTION_URI}administrator${process.env.CONNECTION_SUFFIX}`,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(() => {
         console.log('Successfully connected to the database')
     })
