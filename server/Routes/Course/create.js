a100d9b57ca4da5d771367e9b4ad57cbbe20064a
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-29 13:06:14 +0530

    Added course schema and simple course addtion feature

--- server/Routes/Course/create.js
+++ server/Routes/Course/create.js
@@ -3,7 +3,43 @@ const { courseModel } = require('../../Models/modelIndex')
 
 const router = express.Router()
 
-router.post('/',(req,res) => {
-    
+router.post('/', (req, res) => {
+    let { name, total_marks, isStarted, quiz } = req.body
+    save(name, total_marks, isStarted, quiz)
+        .then((data, err) => {
+            err ? res.json(err) : res.json(data)
+        })
+        .catch(err => {
+            res.json({ ok: false, msg: "An error occured", error: err })
+        })
 })
+
+function save(name, total_marks, isStarted, quiz) {
+    // const ques = [quiz]
+    console.log(quiz)
+    const promise = new Promise((res, rej) => {
+        try {
+
+            courseModel.create({
+                name,
+                total_marks,
+                isStarted,
+                quiz
+            })
+                .then((data, err) => {
+                    if (!err) {
+                        res({ ok: true, msg: "Course Successfully created" })
+                    } else {
+                        rej({ ok: false, msg: "An Error Occured", error: err })
+                    }
+                })
+                .catch(err => {
+                    rej({ok:false,msg:"Error",error:err})
+                })
+        } catch (err) {
+            rej({ ok: false, msg: "An error occured", error: err })
+        }
+    })
+    return promise
+}
 module.exports = router
