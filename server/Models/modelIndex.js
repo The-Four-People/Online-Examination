a90c040d382bc26fa446002f5aa79d3d6d2978ba
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-10-30 13:40:41 +0530

    Small changes

--- server/Models/modelIndex.js
+++ server/Models/modelIndex.js
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 const adminUser = require('./admin');
 const teacherUser = require('./teacher');
 const studentUser = require('./student');
-const courseModel = require('./course');
+// const courseModel = require('./course');
 const courseIndex = require('./courseIndex');
-=======
-const adminUser = require('./admin')
-const teacherUser = require('./teacher')
-const studentUser =  require('./student')
->>>>>>> deeca54a70673031fbc76d6cac6af0d6212c492a
+
 
 module.exports = {
     adminUser,
     teacherUser,
     studentUser,
-<<<<<<< HEAD
-    courseModel,
     courseIndex,
 };
-=======
-}
->>>>>>> deeca54a70673031fbc76d6cac6af0d6212c492a
+
