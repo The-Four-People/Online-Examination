6892e0aaa5e3c0480bc89ee6aa9db65ee6e2709f
string-tejas <jtjtejas05@gmail.com>
2021-10-31 22:55:05 +0530

    id removed from all user tokens

--- server/Middlewares/hasToken.js
+++ server/Middlewares/hasToken.js
@@ -1,22 +1,25 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const path = require('path');
+const { teacherUser } = require('../Models/modelIndex');
 
 dotenv.config({
     path: path.join(__dirname, '../', '.env'),
 });
 
-const hasToken = (req, res, next) => {
+const hasToken = async (req, res, next) => {
     try {
         const auth = req.headers['authorization'];
         const authToken = auth.split(' ')[1];
         const result = jwt.verify(authToken, process.env.key);
-        if (
-            result.role === 'admin' ||
-            result.role === 'student' ||
-            result.role === 'teacher'
-        ) {
+        if (result.role === 'admin' || result.role === 'student') {
             req.obj = result;
+            next();
+        } else if (result.role === 'teacher') {
+            const teacher = await teacherUser
+                .findOne({ email: result.email })
+                .exec();
+            req.obj = teacher;
             next();
         } else {
             res.json({ ok: false, msg: 'hasToken - Unidentified token' });
