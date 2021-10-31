2c3bde00f23db4b92ffb278003e4e5cd5182cc07
string-tejas <jtjtejas05@gmail.com>
2021-10-31 13:28:30 +0530

    Fixes

--- Middlewares/hasToken.js
+++ Middlewares/hasToken.js
@@ -1,35 +1,28 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const path = require('path');
-const { teacherUser } = require('../Models/modelIndex');
 
 dotenv.config({
     path: path.join(__dirname, '../', '.env'),
 });
 
-const hasToken = async (req, res, next) => {
+const hasToken = (req, res, next) => {
     try {
         const auth = req.headers['authorization'];
         const authToken = auth.split(' ')[1];
         const result = jwt.verify(authToken, process.env.key);
-        if (result.role === 'admin' || result.role === 'student') {
+        if (
+            result.role === 'admin' ||
+            result.role === 'student' ||
+            result.role === 'teacher'
+        ) {
             req.obj = result;
             next();
-        } else if (result.role === 'teacher') {
-            await teacherUser.findOne(
-                { email: result.email },
-                (err, teacher) => {
-                    if (!err) {
-                        req.obj = teacher;
-                        next();
-                    } else console.log(err);
-                }
-            );
         } else {
-            res.json({ ok: false, msg: 'Not Authorised' });
+            res.json({ ok: false, msg: 'hasToken - Unidentified token' });
         }
     } catch (err) {
-        // console.log(err);
+        console.log(err);
     }
 };
 module.exports = hasToken;
--- Routes/Login/student.js
+++ Routes/Login/student.js
@@ -1,105 +1,120 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const { studentUser } = require('../../Models/modelIndex')
-const dotenv = require('dotenv')
-const path = require('path')
-const { body, validationResult } = require('express-validator')
-const jwt = require("jsonwebtoken")
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { studentUser } = require('../../Models/modelIndex');
+const dotenv = require('dotenv');
+const path = require('path');
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 dotenv.config({
-    path: path.join(__dirname, '../', '.env')
-})
-
-
+    path: path.join(__dirname, '../', '.env'),
+});
 
 const validate = (req, res, next) => {
-    let errors = validationResult(req)
+    let errors = validationResult(req);
     if (!errors.isEmpty()) {
-        errors = errors.errors
-        console.log(errors)
+        errors = errors.errors;
+        console.log(errors);
         if (errors.length > 1) {
             return res.json({
                 ok: false,
-                msg: "Not a valid email address and password should me minimum 5 chracters" +
-                    "and maximum 20 characters"
-            })
+                msg:
+                    'Not a valid email address and password should me minimum 5 chracters' +
+                    'and maximum 20 characters',
+            });
         } else {
             if (errors[0].param === 'email') {
-                return res.json({ ok: false, msg: "Provide a valid email address" })
+                return res.json({
+                    ok: false,
+                    msg: 'Provide a valid email address',
+                });
             } else {
-                return res.json({ ok: false, msg: "Password should be minimum 5 characters and maximum 20 characters" })
+                return res.json({
+                    ok: false,
+                    msg: 'Password should be minimum 5 characters and maximum 20 characters',
+                });
             }
         }
     }
 
-    next()
-}
+    next();
+};
 
 function findUser(email) {
     const promise = new Promise((resolve, reject) => {
         try {
-            studentUser.findOne({ email: email },
-                function (err, user) {
-                    if (!err) {
-                        resolve(user)
-                    } else {
-                        reject(err)
-                    }
+            studentUser.findOne({ email: email }, function (err, user) {
+                if (!err) {
+                    resolve(user);
+                } else {
+                    reject(err);
                 }
-            )
+            });
         } catch (err) {
-            reject({ ok: false, msg: "Error", error: err })
+            reject({ ok: false, msg: 'Error', error: err });
         }
-    })
-    return promise
+    });
+    return promise;
 }
 
 function checkpassword(password, hash) {
     try {
-        const login = bcrypt.compareSync(password, hash)
+        const login = bcrypt.compareSync(password, hash);
         if (login === true) {
-            return true
+            return true;
         } else {
-            return false
+            return false;
         }
     } catch (err) {
-        console.log(err)
-        return false
+        console.log(err);
+        return false;
     }
 }
 
-
-router.post('/',
+router.post(
+    '/',
     body('email').isEmail(),
     body('password').isLength({ min: 5, max: 20 }),
     validate,
     (req, res) => {
-        let { email, password } = req.body
-        password = password.toString()
+        let { email, password } = req.body;
+        password = password.toString();
         findUser(email, password)
             .then((user, err) => {
                 if (user) {
-                    const isPasswordCorrect = checkpassword(password, user.password)
+                    const isPasswordCorrect = checkpassword(
+                        password,
+                        user.password
+                    );
                     if (isPasswordCorrect) {
-                        const { name, email, role } = user
-                        const jwt_token = jwt.sign({ name: name, email: email, role: role }, process.env.key, { expiresIn: '2h' })
-                        console.log(jwt_token)
-                        res.json({ ok: true, msg: "Student found", token: jwt_token })
+                        const { _id, name, email, role } = user;
+                        const jwt_token = jwt.sign(
+                            { _id: _id, name: name, email: email, role: role },
+                            process.env.key,
+                            { expiresIn: '2h' }
+                        );
+                        console.log(jwt_token);
+                        res.json({
+                            ok: true,
+                            msg: 'Student found',
+                            token: jwt_token,
+                        });
                     } else {
-                        res.json({ ok: false, msg: "Password Incorrect" })
+                        res.json({ ok: false, msg: 'Password Incorrect' });
                     }
                 } else {
-                    res.json({ ok: false, msg: "Student not found" })
+                    res.json({ ok: false, msg: 'Student not found' });
                 }
-            }).
-            catch(err => {
-                res.json(err)
             })
-    })
+            .catch((err) => {
+                res.json(err);
+            });
+    }
+);
 
 router.get('/', (req, res) => {
-    res.sendStatus(404)
-})
+    res.sendStatus(404);
+});
 
-module.exports = router
+module.exports = router;
