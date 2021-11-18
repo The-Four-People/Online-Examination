36baed7b7a7d52924df73caa6818d4d98ed77b6c
string-tejas <jtjtejas05@gmail.com>
2021-11-18 12:32:39 +0530

    Student dashboard added

--- client/src/components/componentIndex.js
+++ client/src/components/componentIndex.js
@@ -1,11 +1,12 @@
-export { default as Navbar } from "./Navbar";
-export { default as Sidebar } from "./Sidebar";
-export { default as Login } from "./login/Login";
-export { default as RegisterAdmin } from "./register/Admin";
-export { default as RegisterStudent } from "./register/Student";
-export { default as DashboardTr } from "./DashboardTr";
-export { default as CourseTr } from "./CourseTr";
-export { default as TestTr } from "./TestTr";
-export { default as TestSt } from "./TestSt";
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Login } from './login/Login';
+export { default as RegisterAdmin } from './register/Admin';
+export { default as RegisterStudent } from './register/Student';
+export { default as DashboardTr } from './DashboardTr';
+export { default as DashboardSt } from './DashboardSt';
+export { default as CourseTr } from './CourseTr';
+export { default as TestTr } from './TestTr';
+export { default as TestSt } from './TestSt';
 
 // use import {ComponentName1, ComponentName2} from './component/componentIndex'
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,27 +1,32 @@
-import React from "react";
-import hasToken from "../methods/hasToken";
-import { Navbar, DashboardTr } from "../components/componentIndex";
+import React from 'react';
+import hasToken from '../methods/hasToken';
+import { Navbar, DashboardTr, DashboardSt } from '../components/componentIndex';
 
 const Dashboard = () => {
-	const [user, setUser] = React.useState({});
-	React.useLayoutEffect(() => {
-		setUser(hasToken());
-	}, []);
-	const decideRender = () => {
-		if (user.role === "teacher") {
-			return (
-				<>
-					<Navbar signIn={!user.ok} />
-					<DashboardTr user={user} />
-				</>
-			);
-		} else if (user.role === "student") {
-			return <h1>Student dashboard</h1>;
-		} else {
-			return <h1>Something went wrong</h1>;
-		}
-	};
-	return decideRender();
+    const [user, setUser] = React.useState({});
+    React.useLayoutEffect(() => {
+        setUser(hasToken());
+    }, []);
+    const decideRender = () => {
+        if (user.role === 'teacher') {
+            return (
+                <>
+                    <Navbar signIn={!user.ok} />
+                    <DashboardTr user={user} />
+                </>
+            );
+        } else if (user.role === 'student') {
+            return (
+                <>
+                    <Navbar signIn={!user.ok} />
+                    <DashboardSt user={user} />
+                </>
+            );
+        } else {
+            return <h1>Something went wrong</h1>;
+        }
+    };
+    return decideRender();
 };
 
 export default Dashboard;
