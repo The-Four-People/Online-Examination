573ffce57ea5a955e61b2b9f264e3fffb7a9dc89
Nadim Shaikh <nadimsahikh@12345678@gmail.com>
2021-11-18 11:08:06 +0530

    Test Creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Home, Dashboard } from './pages/pageIndex';
-import { Login, RegisterStudent } from './components/componentIndex';
+import { Login, RegisterStudent, Test} from './components/componentIndex';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import { Navbar, Sidebar } from "./components/componentIndex";
 
@@ -11,19 +11,19 @@ function App() {
                 <Route exact path='/'>
                     <Home />
                 </Route>
-                <Route exact path='/login'>
+                <Route exact path='/login'> 
                     <Login />
                 </Route>
                 <Route exact path='/register'>
                     <RegisterStudent />
                 </Route>
-                {/* <Route exact path="/:courseid/new">
-
-				</Route>  */}
-                {/* Testing */}
+                <Route exact path="/c/:courseid/new">
+                < Test/> 
+				</Route>
                 <Route exact path='/dashboard'>
                     <Dashboard />
                 </Route>
+                <Route exact path='/'></Route>
             </Switch>
         </BrowserRouter>
     );
--- client/src/components/componentIndex.js
+++ client/src/components/componentIndex.js
@@ -4,5 +4,5 @@ export { default as Login } from './login/Login';
 export { default as RegisterAdmin } from './register/Admin';
 export { default as RegisterStudent } from './register/Student';
 export { default as DashboardTr } from './DashboardTr';
-
+export { default as Test} from './Test';
 // use import {ComponentName1, ComponentName2} from './component/componentIndex'
