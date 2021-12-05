bc4968b5b5e7a3cea023b3da12ed9ea62da666f0
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-12-05 18:18:29 +0530

    Upcoming tests done

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import hasToken from '../methods/hasToken';
-import { Navbar, DashboardTr, DashboardSt } from '../components/componentIndex';
+import React from "react";
+import hasToken from "../methods/hasToken";
+import { Navbar, DashboardTr, DashboardSt } from "../components/componentIndex";
 
 const Dashboard = () => {
-    const [user, setUser] = React.useState({});
-    React.useLayoutEffect(() => {
-        setUser(hasToken());
-    }, []);
-    const decideRender = () => {
-        if (user.role === 'teacher') {
-            return (
-                <>
-                    <Navbar signIn={!user.ok} />
-                    <DashboardTr user={user} />
-                </>
-            );
-        } else if (user.role === 'student') {
-            return (
-                <>
-                    <Navbar signIn={!user.ok} />
-                    <DashboardSt user={user} />
-                </>
-            );
-        } else {
-            return <h1>Something went wrong</h1>;
-        }
-    };
-    return decideRender();
+	const [user, setUser] = React.useState({});
+
+	document.title = "Dashboard | Online Examination";
+	React.useLayoutEffect(() => {
+		setUser(hasToken());
+	}, []);
+	const decideRender = () => {
+		if (user.role === "teacher") {
+			return (
+				<>
+					<Navbar signIn={!user.ok} />
+					<DashboardTr user={user} />
+				</>
+			);
+		} else if (user.role === "student") {
+			return (
+				<>
+					<Navbar signIn={!user.ok} />
+					<DashboardSt user={user} />
+				</>
+			);
+		} else {
+			return <h1>Something went wrong</h1>;
+		}
+	};
+	return decideRender();
 };
 
 export default Dashboard;
