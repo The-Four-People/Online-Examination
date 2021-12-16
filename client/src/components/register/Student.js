9f32f61e7dd48d087f847ce360ec05e18337f4be
Omkar Bhoir <omkarbhoir8778@gmail.com>
2021-12-16 13:09:55 +0530

    Done

--- client/src/components/register/Student.js
+++ client/src/components/register/Student.js
@@ -1,115 +1,106 @@
-import { React, useLayoutEffect, useState, useRef } from 'react';
-import hasToken from '../../methods/hasToken';
-import { Redirect } from 'react-router';
-import { Navbar } from '../componentIndex';
+import { React, useLayoutEffect, useState, useRef } from "react";
+import hasToken from "../../methods/hasToken";
+import { Redirect } from "react-router";
+import { Navbar } from "../componentIndex";
 
 export default function Student() {
-    const [isAuthorise, setisAuthorise] = useState(true);
-    const [msg, setMsg] = useState('');
-    const name = useRef(null);
-    const email = useRef(null);
-    const password = useRef(null);
+	const [isAuthorise, setisAuthorise] = useState(true);
+	const [msg, setMsg] = useState("");
+	const name = useRef(null);
+	const email = useRef(null);
+	const password = useRef(null);
 
-    document.title = 'Register Student | Online Examination';
+	document.title = "Register Student | Online Examination";
 
-    useLayoutEffect(() => {
-        const data = hasToken();
-        // console.log(data);
-        if (data.ok === true && data.role === 'teacher') {
-            setisAuthorise(true);
-        } else {
-            setisAuthorise(false);
-        }
-    }, []);
+	useLayoutEffect(() => {
+		const data = hasToken();
+		// console.log(data);
+		if (data.ok === true && data.role === "teacher") {
+			setisAuthorise(true);
+		} else {
+			setisAuthorise(false);
+		}
+	}, []);
 
-    function handleFormSubmit(e) {
-        e.preventDefault();
-        console.log(JSON.parse(localStorage.getItem('token')));
-        const Name = name.current.value;
-        const Email = email.current.value;
-        const Password = password.current.value;
-        console.log('Registering.....');
-        if (Name && Email && Password) {
-            fetch(`${process.env.REACT_APP_BASE_URI}/api/register/student`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `bearer ${JSON.parse(
-                        localStorage.getItem('token')
-                    )}`,
-                },
-                body: JSON.stringify({
-                    name: Name,
-                    email: Email,
-                    password: Password,
-                }),
-            })
-                .then((data, err) => {
-                    if (!err) {
-                        return data.json();
-                    } else {
-                        console.log(err);
-                    }
-                })
-                .then((data) => {
-                    console.log(data);
-                    setMsg(
-                        data.msg +
-                            '- user with the given email may already exist'
-                    );
-                    if (data.ok === true) {
-                        document.getElementById('form').reset();
-                    }
-                })
-                .catch((err) => console.log(err));
-        }
-    }
+	function handleFormSubmit(e) {
+		e.preventDefault();
+		console.log(JSON.parse(localStorage.getItem("token")));
+		const Name = name.current.value;
+		const Email = email.current.value;
+		const Password = password.current.value;
+		console.log("Registering.....");
+		if (Name && Email && Password) {
+			fetch(`${process.env.REACT_APP_BASE_URI}/api/register/student`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
+				},
+				body: JSON.stringify({
+					name: Name,
+					email: Email,
+					password: Password,
+				}),
+			})
+				.then((data, err) => {
+					if (!err) {
+						return data.json();
+					} else {
+						console.log(err);
+					}
+				})
+				.then((data) => {
+					console.log(data);
+					setMsg(data.msg);
+					if (data.ok === true) {
+						document.getElementById("form").reset();
+					}
+				})
+				.catch((err) => console.log(err));
+		}
+	}
 
-    return (
-        <>
-            {isAuthorise === false ? <Redirect to='/' /> : ''}
-            <Navbar signIn={false} />
-            <div className='heading-sep'>Register Student</div>
-            <div className='main-form-container'>
-                <div className='form-container'>
-                    <form
-                        id='form'
-                        className='form'
-                        onSubmit={handleFormSubmit}
-                    >
-                        <input
-                            name='name'
-                            type='text'
-                            placeholder='Name'
-                            required
-                            maxLength='20'
-                            ref={name}
-                        />
+	return (
+		<>
+			{isAuthorise === false ? <Redirect to="/" /> : ""}
+			<Navbar signIn={false} />
+			<div className="heading-sep">Register Student</div>
+			<div className="main-form-container">
+				<div className="form-container">
+					<form id="form" className="form" onSubmit={handleFormSubmit}>
+						<input
+							name="name"
+							type="text"
+							placeholder="Name"
+							required
+							maxLength="20"
+							ref={name}
+						/>
 
-                        <input
-                            name='email'
-                            type='email'
-                            placeholder='Email'
-                            ref={email}
-                            required
-                        />
+						<input
+							name="email"
+							type="email"
+							placeholder="Email"
+							ref={email}
+							required
+						/>
 
-                        <input
-                            name='password'
-                            type='password'
-                            placeholder='Password'
-                            required
-                            minLength='5'
-                            maxLength='20'
-                            ref={password}
-                            // pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
-                        />
+						<input
+							name="password"
+							type="password"
+							placeholder="Password"
+							required
+							minLength="5"
+							maxLength="20"
+							ref={password}
+							// pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+						/>
 
-                        <input type='submit' value='Register' />
-                        <p className='reg-login'>{msg}</p>
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+						<input type="submit" value="Register" />
+						<p className="reg-login">{msg}</p>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 }
